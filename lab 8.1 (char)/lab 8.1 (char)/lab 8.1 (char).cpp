a94bdb7188a7d814f2f2918284f4c3362944be52
Iryna Prendkovych <iryna.prendkovych.itis.2020@lpnu.ua>
2020-11-23 21:26:38 +0200

    lab 8.1 char

---
+++ lab 8.1 (char)/lab 8.1 (char)/lab 8.1 (char).cpp
@@ -0,0 +1,64 @@
+﻿// lab 8.1 (char).cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+int Count(char* s)
+{
+	int k = 0,
+		pos = 0;
+	char* t;
+	while (t = strchr(s + pos, 'n'))
+	{
+		pos = t - s + 1;
+		if (s[pos + 1] == 'o')
+			k++;
+	}
+	return k;
+}
+char* Change(char* s)
+{
+	char* t = new char[strlen(s)];
+	char* p;
+	int pos1 = 0,
+		pos2 = 0;
+	*t = 0;
+	while (p = strchr(s + pos1, 'n'))
+	{
+		if (s[p - s + 2] == 'n')
+		{
+			pos2 = p - s + 3;
+			strncat(t, s + pos1, pos2 - pos1 - 3);
+			strcat(t, "**");
+			pos1 = pos2;
+		}
+	}
+	strcat(t, s + pos1);
+	strcpy(s, t);
+	return t;
+}
+int main()
+{
+	char str[101];
+	cout << "Enter string:" << endl;
+	cin.getline(str, 100);
+	cout << "String contained " << Count(str) << " groups of '+ -'" << endl;
+	char* dest = new char[151];
+	dest = Change(str);
+	cout << "Modified string (param) : " << str << endl;
+	cout << "Modified string (result): " << dest << endl;
+
+		return 0;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
